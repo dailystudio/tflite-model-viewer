adf45ce87dcce167b00349494b2323ef6b7db8b6
Nan YE <dailystudio2010@gmail.com>
2021-02-18 23:42:29 +0800

    [android][DEV]: first workable version with bird model;

--- android/app/src/main/java/com/dailystudio/tflite/viewer/MainActivity.kt
+++ android/app/src/main/java/com/dailystudio/tflite/viewer/MainActivity.kt
@@ -1,13 +1,60 @@
 package com.dailystudio.tflite.viewer
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.dailystudio.tflite.example.common.AbsExampleActivity
+import com.dailystudio.tflite.example.common.image.ImageInferenceInfo
+import com.dailystudio.tflite.viewer.fragment.LiteModelViewerFragment
+import org.tensorflow.lite.support.label.Category
+import kotlin.math.min
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AbsExampleActivity<ImageInferenceInfo, List<Category>>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    companion object {
+        const val REPRESENTED_ITEMS_COUNT = 3
+    }
+
+    private var detectItemViews: Array<TextView?> =
+        Array(REPRESENTED_ITEMS_COUNT) {null}
+    private var detectItemValueViews: Array<TextView?> =
+        Array(REPRESENTED_ITEMS_COUNT) {null}
+
+    override fun createBaseFragment(): Fragment {
+        return LiteModelViewerFragment()
+    }
+
+    override fun createResultsView(): View? {
+        val resultsView = LayoutInflater.from(this).inflate(
+            R.layout.layout_results, null)
+
+        resultsView?.let {
+            setupResultView(it)
+        }
+
+        return resultsView
+    }
+
+    private fun setupResultView(resultsView: View) {
+        for (i in 0 until REPRESENTED_ITEMS_COUNT) {
+            detectItemViews[i] = resultsView.findViewById(
+                resources.getIdentifier("detected_item${i + 1}", "id", packageName)
+            )
+
+            detectItemValueViews[i] = resultsView.findViewById(
+                resources.getIdentifier("detected_item${i + 1}_value", "id", packageName)
+            )
+        }
+    }
+
+    override fun onResultsUpdated(results: List<Category>) {
+        val itemCount = min(results.size, REPRESENTED_ITEMS_COUNT)
+
+        for (i in 0 until itemCount) {
+            detectItemViews[i]?.text = results[i].label
+            detectItemValueViews[i]?.text = "%.1f%%".format(results[i].score * 100)
+        }
     }
 
 }
